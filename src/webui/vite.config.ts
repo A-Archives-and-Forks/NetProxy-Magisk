cb69dabb3ea09cc75bd8cd6fb13629ba3fae109d
Fanju <98313711+Fanju6@users.noreply.github.com>
2026-01-07 13:14:06 +0800

    优化一些代码

--- src/webui/vite.config.ts
+++ src/webui/vite.config.ts
@@ -4,7 +4,19 @@ export default defineConfig({
     build: {
         outDir: '../module/webroot',
         emptyOutDir: true,
-        sourcemap: false
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('mdui')) {
+                            return 'mdui';
+                        }
+                        return 'vendor';
+                    }
+                }
+            }
+        }
     },
     server: {
         port: 1234,
